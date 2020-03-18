173b4822d8027864275ce3c973e5234d8e23903f
Carlos Reynosa <carlos.reynosa@pdp.com>
2020-03-17 17:11:32 -0700

    Initial commit.

--- bin/cloud/bash-boilerplate-template.sh
+++ bin/cloud/bash-boilerplate-template.sh
@@ -15,7 +15,7 @@
 # You are not obligated to bundle the LICENSE file with your b3bp projects as long
 # as you leave these references intact in the header comments of your source files.
 
-function FUNCTION_NAME () {
+function get-latest-config () {
 
 
 # Exit on error. Append "|| true" if you expect an error.
@@ -444,8 +444,8 @@ debug "Destination Option: ${environmentOption}"
 }
 
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-  export -f FUNCTION_NAME
+  export -f get-latest-config
 else
-  FUNCTION_NAME "${@}"
+  get-latest-config "${@}"
   exit ${?}
 fi
